59d2e1d93b70350a0278251159d4640afcf7ea9f
SREENIVASA <SREENIVASA@SREENIVAS>
2017-08-24 14:19:39 +0530

    commit 2

--- src/main/java/pageClasses/HomePage.java
+++ src/main/java/pageClasses/HomePage.java
@@ -1,22 +1,28 @@
 package pageClasses;
 
+import java.sql.Driver;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.annotations.Test;
 
-public class HomePage {
+import testBase.TestBase;
+
+public class HomePage extends TestBase{
 	
 	
-	@FindBy(xpath="//*[@id='header']/div[2]/div/div/nav/div[1]/a")
-	WebElement signInButton;
+	//@FindBy(xpath="//*[@id='header']/div[2]/div/div/nav/div[1]/a")
+	WebElement signInButton=driver.findElement(By.xpath("//*[@id='header']/div[2]/div/div/nav/div[1]/a"));
 	
 	
 	
 	
 	
-	@Test
+	
 	public void clickOnSignInButton(){
 		signInButton.click();
+		System.out.println("clicked on signIn Button:)");
 	}
 
-}
+}//initElements
